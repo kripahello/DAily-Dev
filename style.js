e4990d7aa51af99bc4b5a772b21003b72662b501
kripa <kripapokharel7@gmail.com>
2022-08-01 00:01:20 +0545

    button-hover-property-added and some-edits

--- style.js
+++ style.js
@@ -1,14 +1,18 @@
-const html=`
-<div class="card" style="width: 18rem;">
-<img src="..." class="card-img-top" alt="...">
-<div class="card-body">
-  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-</div>
-</div>`
+// function mOver(obj) {
+//   obj.style.color ="red";
+// }
+
+// const html=`
+// <div class="card" style="width: 18rem;">
+// <img src="..." class="card-img-top" alt="...">
+// <div class="card-body">
+//   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+// </div>
+// </div>`
 
 
 
-for(let i=0;i<15;i++)
-{
-document.getElementById("check").innerHTML=html;
-}
+// for(let i=0;i<15;i++)
+// {
+// document.getElementById("check").innerHTML=html;
+// }
